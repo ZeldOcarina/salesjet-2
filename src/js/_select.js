5d59f58b0b7add316214edea8b5ae8c7886f3890
unknown <mattia.r86@gmail.com>
2021-06-25 18:16:09 +0200

    Complete mobile version

--- src/js/_select.js
+++ src/js/_select.js
@@ -1,10 +1,16 @@
 export default class Select {
   constructor(select) {
     this.select = select;
-    this.select.addEventListener("change", this.handleSelectChange.bind(this));
+    // this.select.addEventListener("change", this.handleSelectChange.bind(this));
+    // this.select.addEventListener(
+    //   "focusout",
+    //   this.handleSelectChange.bind(this),
+    //   true
+    //);
   }
 
   handleSelectChange(e) {
+    alert("FIRING!!!!");
     return window.location.assign(`/${e.target.value}`);
   }
 }
